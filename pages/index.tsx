67beb659b82801577f10eb2edb4872ce5049ce3f
Hiago Coelho <b8coelho@gmail.com>
2022-04-08 01:30:33 -0300

    [init]: first commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>tech-calendar: home</title>
+        <meta name="description" content="reminders for your tech related issues." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <p>tech-calendar</p>
+
+    </div>
+  )
+}
+
+export default Home
